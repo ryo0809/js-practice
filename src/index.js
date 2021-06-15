17b327a0fde15a3a656c34e05a9272daea0ecb03
ryo fujiwara <ryo.tanagp1@gmail.com>
2021-06-16 00:35:05 +0900

    追加、削除機能を実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // 入力値を取得して値を初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  // 未完了のTODO要素
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.className = "list-row";
+  const p = document.createElement("p");
+  p.className = "list-title";
+  p.innerText = inputText;
+  // ボタン要素
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親要素ごと未完了リストから削除
+    const deleteTarget = deleteButton.parentNode.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  li.appendChild(div);
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  console.log(div);
+  // 未完了リストに追加
+  const ul = document.getElementById("incomplete-list");
+  ul.appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
