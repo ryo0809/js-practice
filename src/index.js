e9a79b90f7b564e03f45bec240071012a5719ca2
ryo fujiwara <ryo.tanagp1@gmail.com>
2021-06-16 15:04:18 +0900

    共通処理をメソッド化、コメント追加

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import "./styles.css";
 
+// 追加ボタンのクリックイベント
 const onClickAdd = () => {
   // 入力値を取得して値を初期化
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  // 未完了のTODO要素
-  const li = document.createElement("li");
-  const div = document.createElement("div");
-  div.className = "list-row";
-  const p = document.createElement("p");
-  p.className = "list-title";
-  p.innerText = inputText;
+
   // ボタン要素
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
+    // 戻すボタン
+    const returnButton = document.createElement("button");
+    returnButton.innerText = "戻す";
+    // 完了したTODOから未完了のTODOへ戻す
+    returnButton.addEventListener("click", () => {
+      const deleteTarget = returnButton.parentNode.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+      moveElement(
+        returnButton.previousElementSibling.innerText,
+        "incomplete-list"
+      );
+    });
     // 未完了のTODOから完了したTODOへ要素を移動させる
     const completeTarget = completeButton.parentNode.parentNode;
     document.getElementById("incomplete-list").removeChild(completeTarget);
@@ -24,47 +31,39 @@ const onClickAdd = () => {
     const p = document.createElement("p");
     p.className = "list-title";
     p.innerText = completeButton.previousElementSibling.innerText;
-    const returnButton = document.createElement("button");
-    returnButton.innerText = "戻す";
-    returnButton.addEventListener("click", () => {
-      // 完了したTODOから未完了のTODOへ戻す
-      const deleteTarget = returnButton.parentNode.parentNode;
-      document.getElementById("complete-list").removeChild(deleteTarget);
-      const li = document.createElement("li");
-      const div = document.createElement("div");
-      div.className = "list-row";
-      const p = document.createElement("p");
-      p.className = "list-title";
-      p.innerText = returnButton.previousElementSibling.innerText;
-      li.appendChild(div);
-      div.appendChild(p);
-      div.appendChild(completeButton);
-      div.appendChild(deleteButton);
-      // 未完了リストに追加
-      const ul = document.getElementById("incomplete-list");
-      ul.appendChild(li);
-    });
     li.appendChild(div);
     div.appendChild(p);
     div.appendChild(returnButton);
     document.getElementById("complete-list").appendChild(li);
   });
 
+  // 削除ボタン
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
+  // 押された削除ボタンの親要素ごと未完了リストから削除
   deleteButton.addEventListener("click", () => {
-    // 押された削除ボタンの親要素ごと未完了リストから削除
     const deleteTarget = deleteButton.parentNode.parentNode;
     document.getElementById("incomplete-list").removeChild(deleteTarget);
   });
 
-  li.appendChild(div);
-  div.appendChild(p);
-  div.appendChild(completeButton);
-  div.appendChild(deleteButton);
-  // 未完了リストに追加
-  const ul = document.getElementById("incomplete-list");
-  ul.appendChild(li);
+  // 要素の移動メソッド
+  const moveElement = (moveText, moveElementId) => {
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    div.className = "list-row";
+    const p = document.createElement("p");
+    p.className = "list-title";
+    p.innerText = moveText;
+    li.appendChild(div);
+    div.appendChild(p);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
+    // IDで指定した要素の子要素に追加
+    const ul = document.getElementById(moveElementId);
+    ul.appendChild(li);
+  };
+  // 未完了のTODO要素へ追加
+  moveElement(inputText, "incomplete-list");
 };
 
 document
